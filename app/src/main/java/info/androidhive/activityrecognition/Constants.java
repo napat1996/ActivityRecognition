942a47bf8509b3ba02a97fe832102994d47019c5
napat1996 <napat1996@gmail.com>
2018-07-12 02:59:37 +0900

    correct time with fcking on vehicle

--- app/src/main/java/info/androidhive/activityrecognition/Constants.java
+++ app/src/main/java/info/androidhive/activityrecognition/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
 
-    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 2000;
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 3000;
 
     public static final int CONFIDENCE = 70;
 }
--- app/src/main/java/info/androidhive/activityrecognition/MainActivity.java
+++ app/src/main/java/info/androidhive/activityrecognition/MainActivity.java
@@ -110,7 +110,8 @@ public class MainActivity extends AppCompatActivity{
     }
 
     String newLabel = "";
-    int count1 = 0,count2 = 0,count3 = 0,count4 = 0,count5 = 0,count6 = 0,count7 = 0,count8 = 0,count = 0;
+    String label="";
+    int count1 = 0,count2 = 0,count3 = 0,count4 = 0,count5 = 0,count6 = 0,count7 = 0,count = 0;
 
     private void handleUserActivity(int type, int confidence) {
         Calendar calendar = Calendar.getInstance();
@@ -121,28 +122,18 @@ public class MainActivity extends AppCompatActivity{
 
         String strTime = currentTimestamp.toString();
         startTime = strTime;
-        String label = getString(R.string.activity_unknown);
-
-        DatabaseReference testDB = mRootref.child("FUCKKKK");
-        testDB.child("FFFFFFFF").setValue("UUUU");
 
         int icon = R.drawable.ic_still;
 
             switch (type) {
                 case DetectedActivity.IN_VEHICLE: {
-                    label = getString(R.string.activity_in_vehicle);
+                    newLabel = getString(R.string.activity_in_vehicle);
                     icon = R.drawable.ic_driving;
-                    count1 = count;
-                    count1++;
-                    count = count1;
                     break;
                 }
                 case DetectedActivity.ON_BICYCLE: {
-                    label = getString(R.string.activity_on_bicycle);
+                    newLabel = getString(R.string.activity_on_bicycle);
                     icon = R.drawable.ic_on_bicycle;
-                    count2 = count;
-                    count2++;
-                    count = count2;
                     break;
                 }
 //            case DetectedActivity.ON_FOOT: {
@@ -153,51 +144,33 @@ public class MainActivity extends AppCompatActivity{
 //                break;
 //            }
                 case DetectedActivity.RUNNING: {
-                    label = getString(R.string.activity_running);
+                    newLabel = getString(R.string.activity_running);
                     icon = R.drawable.ic_running;
-                    count4 = count;
-                    count4++;
-                    count = count4;
                     break;
                 }
                 case DetectedActivity.STILL: {
-                    label = getString(R.string.activity_still);
+                    newLabel = getString(R.string.activity_still);
                     icon = R.drawable.ic_still;
-                    count5 = count;
-                    count5++;
-                    count = count5;
                     break;
                 }
                 case DetectedActivity.TILTING: {
-                    label = getString(R.string.activity_tilting);
+                    newLabel  = getString(R.string.activity_tilting);
                     icon = R.drawable.ic_tilting;
-                    count6 = count;
-                    count6++;
-                    count = count6;
                     break;
                 }
                 case DetectedActivity.WALKING: {
-                    label = getString(R.string.activity_walking);
+                    newLabel = getString(R.string.activity_walking);
                     icon = R.drawable.ic_walking;
-                    count7 = count;
-                    count7++;
-                    count = count7;
                     break;
                 }
-                case DetectedActivity.UNKNOWN: {
-                    label = getString(R.string.activity_unknown);
-                    count8 = count;
-                    count8++;
-                    count = count8;
-                    break;
-                }
+//                case DetectedActivity.UNKNOWN: {
+//                    label = getString(R.string.activity_unknown);
+//                    break;
+//                }
             }
 
             //String strTime = currentTimestamp.toString();
 
-        if(label == "Unknown"){
-
-        }
 
             Log.d(TAG, "handleUserActivity: " + "Current Timestamp: " + currentTimestamp + " User activity: " + label + ", Confidence: " + confidence + " Count: " + count);
             if (confidence > Constants.CONFIDENCE) {
@@ -206,36 +179,105 @@ public class MainActivity extends AppCompatActivity{
                 txtTime.setText("Time: " + currentTimestamp);
                 imgActivity.setImageResource(icon);
 
-                do {
 //                Log.d(TAG, "START : "+startTime+" STOP : "+endTime);
-//                if (startTime.equals(endTime) ){
-                    DatabaseReference childRef = mRootref.child(label);
-                    mRootref.child("0Time").push().setValue(strTime + " " + confidence);
-                    mRootref.child("0Type").push().setValue(label + " " + confidence);
-//                   childRef.child(count + "startTime ").setValue(startTime); //add start Time to DB
+               if (label != newLabel) {
+                   DatabaseReference oldRef = mRootref.child(label);
+                   Log.e(TAG, "****************************************** old: "+label+" new: "+newLabel );
+                   if(label == getString(R.string.activity_in_vehicle)){
+                       oldRef.child(count1 + "stopTime ").setValue(currentTimestamp.toString()); //add start Time to DB
+                   }else if(label == getString(R.string.activity_on_bicycle)){
+                       oldRef.child(count2 + "stopTime ").setValue(currentTimestamp.toString());
+                   }else if(label == getString(R.string.activity_running)){
+                       oldRef.child(count4 + "stopTime ").setValue(currentTimestamp.toString());
+                   }else if(label == getString(R.string.activity_still)){
+                       oldRef.child(count5 + "stopTime ").setValue(currentTimestamp.toString());
+                   }else if(label == getString(R.string.activity_tilting)){
+                       oldRef.child(count6 + "stopTime ").setValue(currentTimestamp.toString());
+                   }else if(label == getString(R.string.activity_walking)){
+                       oldRef.child(count7 + "stopTime ").setValue(currentTimestamp.toString());
+                   }
+                    label = newLabel;
+                   DatabaseReference childRef = mRootref.child(newLabel);
+                   endTime = currentTimestamp.toString();
+                   switch (newLabel) {
+                       case "In Vehicle": {
+                           label = getString(R.string.activity_in_vehicle);
+                      //     DatabaseReference newChildRef = mRootref.child(newLabel);
+                           Log.e(TAG, "newlabel: " + newLabel);
+                         //  newChildRef.child(count1 + "stopTime ").setValue(startTime); //add start Time to DB
+                           count1++;
+                           childRef.child(count1 + "startTime ").setValue(endTime);
+                            newLabel = label;
+                           break;
+                       }
+                       case "On Bicycle": {
+                           label = getString(R.string.activity_on_bicycle);
+                        //   DatabaseReference newChildRef = mRootref.child(newLabel);
+                           Log.e(TAG, "newlabel: " + newLabel);
+                         //  newChildRef.child(count2 + "stopTime ").setValue(startTime); //add start Time to DB
+                           count2++;
+                           childRef.child(count2 + "startTime ").setValue(endTime);
+                           newLabel = label;
+                           break;
+                       }
+                       case "Running": {
+                           label = getString(R.string.activity_running);
+                       //    DatabaseReference newChildRef = mRootref.child(newLabel);
+                           Log.e(TAG, "newlabel: " + newLabel);
+                         //  newChildRef.child(count4 + "stopTime ").setValue(startTime); //add start Time to DB
+                           count4++;
+                           childRef.child(count4 + "startTime ").setValue(endTime);
+                           newLabel = label;
+                           break;
+                       }
+                       case "Still": {
+                           label = getString(R.string.activity_still);
+                    //       DatabaseReference newChildRef = mRootref.child(newLabel);
+                           Log.e(TAG, "newlabel: " + newLabel);
+                         //  newChildRef.child(count5 + "stopTime ").setValue(startTime); //add start Time to DB
+                           count5++;
+                           childRef.child(count5 + "startTime ").setValue(endTime);
+                           newLabel = label;
+                           break;
+                       }
+                       case "Tilting": {
+                           label = getString(R.string.activity_tilting);
+                       //    DatabaseReference newChildRef = mRootref.child(newLabel);
+                           Log.e(TAG, "newlabel: " + newLabel);
+                        //   newChildRef.child(count6 + "stopTime ").setValue(startTime); //add start Time to DB
+                           count6++;
+                           childRef.child(count6 + "startTime ").setValue(endTime);
+                           newLabel = label;
+                           break;
+                       }
+                       case "walking": {
+                           label = getString(R.string.activity_walking);
+                       //    DatabaseReference newChildRef = mRootref.child(newLabel);
+                           Log.e(TAG, "newlabel: " + newLabel);
+                        //   newChildRef.child(count7 + "stopTime ").setValue(startTime); //add start Time to DB
+                           count7++;
+                           childRef.child(count7 + "startTime ").setValue(endTime);
+                           newLabel = label;
+                           break;
+                       }
+                   }
+
+                   //add stop Time to DB
+                   //mRootref.child("0Time").push().setValue(strTime + " " + confidence);
+                   //mRootref.child("0Type").push().setValue(label + " " + confidence);
+
 //                    Log.e(TAG, "START Timestamp: " + startTime + " User activity: " + label + ", Confidence: " + confidence + " Count:  " + count);
-//                }
-//                else {
-                    endTime = currentTimestamp.toString();
-                    childRef.child(count + "stopTime ").setValue(endTime); //add stop Time to DB
-//
+               }
+
+
                     Log.e(TAG, "STOP Timestamp: " + endTime
                             + " User activity: " + label + ", Confidence: " + confidence +" Count: "+count);
-                    newLabel = label;
-                    count++;
+
+                 //   count++;
 //                    startTime = endTime;
 //                    Log.d(TAG, "start time: " + strTime +" newLabel"+ newLabel);
-//                }
-
-                }
-
-                while(label  != newLabel);
-
-
+            }
                 //endTime = strTime;
-                count--;
-
-                endTime = currentTimestamp.toString();
                 Log.d(TAG, "start: "+ startTime+" stop: "+endTime);
 
             }
@@ -245,7 +287,6 @@ public class MainActivity extends AppCompatActivity{
 
 
 
-    }
 
     @Override
     protected void onResume() {
